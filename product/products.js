dd02c108236ae047fec39d0551af5ec84cdf6c3f
Joseph Davis <joethebartenderpdx@gmail.com>
2021-03-30 15:52:39 -0700

    added some styling

--- product/products.js
+++ product/products.js
@@ -1,8 +1,6 @@
 import { wands } from '../products.js';
 import { createWandLi } from '../utils.js';
 
-console.log(wands);
-
 const ul = document.querySelector('.products');
 
 for (let wand of wands) {
